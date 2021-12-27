0e13789695836cb36a28221c937ff51926d5fc13
calvinhofman <60505346+calvinhofman@users.noreply.github.com>
2021-12-27 15:43:58 +0100

    SKKRT

---
+++ pages/index.js
@@ -0,0 +1,77 @@
+import "../styles/Home.module.scss";
+import React from "react";
+import Webcam from "react-webcam";
+
+export default function Home() {
+  const webcamRef = React.useRef(null);
+  const mediaRecorderRef = React.useRef(null);
+  const [capturing, setCapturing] = React.useState(false);
+  const [recordedChunks, setRecordedChunks] = React.useState([]);
+
+  // for making a video
+  const handleStartCaptureClick = React.useCallback(() => {
+    setCapturing(true);
+    mediaRecorderRef.current = new MediaRecorder(webcamRef.current.stream, {
+      mimeType: "video/webm",
+    });
+    mediaRecorderRef.current.addEventListener(
+      "dataavailable",
+      handleDataAvailable
+    );
+    mediaRecorderRef.current.start();
+  }, [webcamRef, setCapturing, mediaRecorderRef]);
+
+  const handleDataAvailable = React.useCallback(
+    ({ data }) => {
+      if (data.size > 0) {
+        setRecordedChunks((prev) => prev.concat(data));
+      }
+    },
+    [setRecordedChunks]
+  );
+
+  const handleStopCaptureClick = React.useCallback(() => {
+    mediaRecorderRef.current.stop();
+    setCapturing(false);
+  }, [mediaRecorderRef, webcamRef, setCapturing]);
+
+  const handleDownload = React.useCallback(() => {
+    if (recordedChunks.length) {
+      const blob = new Blob(recordedChunks, {
+        type: "video/webm",
+      });
+      const url = URL.createObjectURL(blob);
+      const a = document.createElement("a");
+      document.body.appendChild(a);
+      a.style = "display: none";
+      a.href = url;
+      a.download = "react-webcam-stream-capture.webm";
+      a.click();
+      window.URL.revokeObjectURL(url);
+      setRecordedChunks([]);
+    }
+  }, [recordedChunks]);
+
+  //for taking a picture
+  const [imgSrc, setImgSrc] = React.useState(null);
+  const capture = React.useCallback(() => {
+    const imageSrc = webcamRef.current.getScreenshot();
+    setImgSrc(imageSrc);
+  }, [webcamRef, setImgSrc]);
+
+  return (
+    <>
+    {imgSrc &&  <img src={imgSrc} />}
+      <Webcam audio={false} ref={webcamRef} screenshotFormat="image/jpeg" />
+      {capturing ? (
+        <button onClick={handleStopCaptureClick}>Stop de opname</button>
+      ) : (
+        <button onClick={handleStartCaptureClick}>Start de opname</button>
+      )}
+      {recordedChunks.length > 0 && (
+        <button onClick={handleDownload}>download de video</button>
+      )}
+      <button onClick={capture}>Neem een foto</button>
+    </>
+  );
+}
